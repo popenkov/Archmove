29a2a1717155b2285f0973082c84040b9a4973f1
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-05-04 09:50:26 +0300

    before new gulp

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,60 @@
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.our-work__slider-next ',
+        prevEl: '.our-work__slider-previous ',
+      },
+
+      spaceBetween: 10,
+
+
+      initialSlide: 0,
+
+
+
+      speed: 800,
+
+      height: 500,
+
+      breakpoints: {
+        '@0.45': {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        '@0.75': {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        /* '@1.60': {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        }, */
+      }
+
+
+});
+
+//swiper reviews
+new Swiper('.reviews-slider', {
+    navigation: {
+        nextEl: '.reviews-slider__toggle--next',
+        prevEl: '.reviews-slider__toggle--before',
+      },
+
+      autoHeight: true,
+
+      initialSlide: 0,
+
+      slidesPerView: 1,
+
+      speed: 800,
+
+
+});
+
+
+
+
+
 //hamburger
 let ESCAPE_BUTTON = '27';
 
